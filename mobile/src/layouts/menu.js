b701911cdc95c221306132bb8380365498963329
wWkarlosWw <129088649+wWkarlosWw@users.noreply.github.com>
2023-09-27 23:26:09 -0400

    menu.js

--- mobile/src/layouts/menu.js
+++ mobile/src/layouts/menu.js
@@ -1,29 +1,20 @@
 import { View, Text, StyleSheet, Button } from 'react-native'
 import React from 'react'
-import { NormalButton, SquareButton, RectButton } from './Button'
+import { NormalButton, SquareButton, RectButton, LowButton, SquareButtona, SquareButtone, SquareButton1, LowButton1} from './Button'
 
 const Menu = () => {
     return (
         <View style={styles.container}>
            <View>
-            <NormalButton  onPress={()=>alert('acabas de oprimir el botonde panico boton de panico')} text ="BOTON DE PANICO"/>
-           </View>
-           
-                <View style={styles.container2}>
-                <SquareButton onPress={()=>alert('acabas de oprimir el botonde panico boton de ASISTENCIA')} text ="ASISTENCIA"/>
-                <SquareButton onPress={()=>alert('acabas de oprimir el botonde panico boton de EMERGENCIA')} text ="EMERGENCIA"/>    
-                </View>                 
-                <View style={styles.container2}>
-                <SquareButton onPress={()=>alert('acabas de oprimir el botonde panico boton de EMERGENCIA')} text ="VIOLENTOMETRO"/>
-                <SquareButton onPress={()=>alert('acabas de oprimir el botonde panico boton de EMERGENCIA')} text ="MAPA"/>
-
+            <NormalButton onPress={()=>alert('acabas de oprimir el botonde panico boton de panico')} text ="BOTON DE PANICO"/>
            </View>
            <View style={styles.container2}>
            <RectButton onPress={()=>alert('acabas de oprimir el botonde panico boton de IDA')} text ="EN CAMINO"/>
            <RectButton onPress={()=>alert('acabas de oprimir el botonde panico boton de ESTOY BIEN')} text ="ESTOY BIEN"/>
            </View>
-           <View>
-
+           <View style={styles.container2}>
+           <LowButton1 onPress={()=>alert('acabas de oprimir el botonde panico boton de zona?')} text =""/>
+           <LowButton onPress={()=>alert('acabas de oprimir el botonde panico boton de perfil')} text =""/> 
            </View>
         </View>
     )
@@ -33,12 +24,12 @@ export default Menu
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'colum', 
-        justifyContent: 'center', 
-        padding:0,
+        flex: 1,
+        justifyContent: 'space-evenly', 
+        borderRadius:10,       
       },
       container2: {
         flexDirection: 'row', 
-        justifyContent: 'center',  
+        justifyContent: 'space-evenly',  
       },
   });
